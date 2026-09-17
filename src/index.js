b941a542c8d43b1b5e37bd745391bc9910bd27c6
idbot2596-sys <idbot2596@gmail.com>
2026-09-17 15:27:35 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/patch.bytes") {
+      return env.ASSETS.fetch(request);
+    }
+
+    return new Response("NineX patch delivery worker", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8"
+      }
+    });
+  }
+};
